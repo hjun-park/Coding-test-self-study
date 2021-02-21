2137055da93d9daff2ae3b159eb4a13e0f60acb5
hjun-park <phj0860@gmail.com>
2021-02-21 15:41:52 +0900

    2021-02-20

--- Programmers/Greedy/체육복.py
+++ Programmers/Greedy/체육복.py
@@ -6,20 +6,20 @@ def my_solution(n, lost, reserve):
     pe_list = [1 for _ in range(0, n)]
 
     for i in lost:
-        pe_list[i-1] -= 1
+        pe_list[i - 1] -= 1
 
     for j in reserve:
-        pe_list[j-1] += 1
+        pe_list[j - 1] += 1
 
     for k in range(len(pe_list)):
         if pe_list[k] > 1:
-            if k > 0 and pe_list[k-1] == 0:
+            if k > 0 and pe_list[k - 1] == 0:
                 pe_list[k] -= 1
-                pe_list[k-1] += 1
+                pe_list[k - 1] += 1
                 continue
-            elif k < len(pe_list)-1 and pe_list[k+1] == 0:
+            elif k < len(pe_list) - 1 and pe_list[k + 1] == 0:
                 pe_list[k] -= 1
-                pe_list[k+1] += 1
+                pe_list[k + 1] += 1
                 continue
 
     for i in range(len(pe_list)):
@@ -32,15 +32,17 @@ def my_solution(n, lost, reserve):
 '''
  다른 사람의 풀이
 '''
+
+
 def other_solution(n, lost, reserve):
-     # 1. 차집합을 이용해서 풀었음
+    # 1. 차집합을 이용해서 풀었음
     _reserve = [r for r in reserve if r not in lost]
     _lost = [l for l in lost if l not in reserve]
 
-     # 2. 체육복 여벌 옷이 있는 사람의 앞 뒤 ( f, b ) 를 지정
-     #     여벌 옷 있는 사람의 앞을 먼저 보고 빼는데 그 사람이 체육복을 가지고 있다면,
-     #     뒤에 사람이 체육복 가지고 있는지 확인해보고 뺀다. 앞뒤로 가지고 있다면 체육복을 가지고 있는게 아니니까 remove를 하지 않음
-     #     그렇게 해서 전체 체육복 가지고 있는 사람 - 제거되지 않은 lost 수를 빼면 answer 출력
+    # 2. 체육복 여벌 옷이 있는 사람의 앞 뒤 ( f, b ) 를 지정
+    #     여벌 옷 있는 사람의 앞을 먼저 보고 빼는데 그 사람이 체육복을 가지고 있다면,
+    #     뒤에 사람이 체육복 가지고 있는지 확인해보고 뺀다. 앞뒤로 가지고 있다면 체육복을 가지고 있는게 아니니까 remove를 하지 않음
+    #     그렇게 해서 전체 체육복 가지고 있는 사람 - 제거되지 않은 lost 수를 빼면 answer 출력
     for r in _reserve:
         f = r - 1
         b = r + 1
@@ -57,10 +59,8 @@ if __name__ == '__main__':
     lost = [2, 4]
     reserve = [3]
     n = 5
-    answer1=my_solution(n, lost, reserve)
-    answer2=other_solution(n, lost, reserve)
+    answer1 = my_solution(n, lost, reserve)
+    answer2 = other_solution(n, lost, reserve)
 
     print("return : ", answer1)
     print("return : ", answer2)
-
-
--- Tips.py
+++ Tips.py
@@ -10,3 +10,17 @@ for i in range(n):
 
 # 3. 두 번째 요소 기준으로 정렬
 s = sorted(s, key=lambda a: a[1])
+
+# 4. 리스트 생성 시 일정한 개수만큼 생성해주고 싶을 때
+solution_list = [ 'A' for _ in range(len(input_list))]
+
+# 5. 차집합을 이용해서 리스트 생성
+_reserve = [r for r in reserve if r not in lost]
+
+
+# 6. 중복제거하고 정렬
+d2 = list(set(d))
+
+# 7. 중복제거하고 정렬하지 않기
+from collections import OrderedDict
+list( OrderedDict.fromkeys(d).keys())
