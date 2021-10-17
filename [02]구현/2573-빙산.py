f26a94f80c1f32bf9b8c19902caf7f4fccbd92c1
hjun-park <phj0860@gmail.com>
2021-10-17 21:51:57 +0900

    [2021-10-17] 구현

--- [02]구현/2573-빙산.py
+++ [02]구현/2573-빙산.py
@@ -1,104 +1,80 @@
-import sys
 from collections import deque
+import copy
+import sys
 
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-melted_graph = [[0] * M for _ in range(N)]
+graph = [list(map(int, input().split())) for i in range(N)]
 
-# 구현
-# 1) 얼음 덩어리가 몇 개인지 확인
-# 2) 1년 후 빙하가 녹은 것 계산
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-dx = [0, -1, 0, 1]
-dy = [-1, 0, 1, 0]
+year = 0  # 년도
 
 
-# 2) 1년 후 빙하가 녹은 것 계산
-def melt(x, y):
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
+# 1년 후의 빙산 형태로 반환
+def after_iceberg(graph):  # 섬의 숫자 줄이기
+    copy_graph = copy.deepcopy(graph)
 
-        if graph[nx][ny] > 0:
-            graph[nx][ny] -= 1
+    for x in range(0, N):
+        for y in range(0, M):
+            zero_zone = 0
+            if graph[x][y] > 0:     # 빙산 값이 있는 경우 4방향 체크하여 비어있는 부분 확인
+                for d in range(4):
+                    nx = x + dx[d]
+                    ny = y + dy[d]
 
-    return 1
+                    if 0 <= nx < N and 0 <= ny < M:
+                        if graph[nx][ny] == 0:
+                            zero_zone += 1
 
-
-def melt_v2():
-    print(f'melted_graph {melted_graph}')
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] != 0:
-                graph[i][j] -= melted_graph[i][j]
-
-    print(graph)
-
-
-def melt_v3(*graphs):
-    return [[row[0] * 2 - sum(row) for row in zip(*t)] for t in zip(*graphs)]
+                if graph[x][y] - zero_zone <= 0:    # 빙산은 음수의 경우 0로 치환
+                    copy_graph[x][y] = 0
+                elif graph[x][y] - zero_zone > 0:   # 아닌 경우 그냥 뺸 값으로 저장
+                    copy_graph[x][y] = graph[x][y] - zero_zone
+    return copy_graph
 
 
-# BFS 돌면서 0이 몇 개인지 따로 기록
-def bfs(x, y):
-    is_visit = False
+# 섬의 개수를 세는 함수
+def bfs(a, b):
     q = deque()
-    q.append((x, y))
-    visited[x][y] = True
+    q.append((a, b))
+    visited[a][b] = True
 
     while q:
-        qx, qy = q.popleft()
+        x, y = q.popleft()
 
-        for d in range(4):
-            nx = qx + dx[d]
-            ny = qy + dy[d]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
             if 0 <= nx < N and 0 <= ny < M:
-                if not visited[nx][ny] and graph[nx][ny] != 0:
-                    is_visit = True
+                if not visited[nx][ny] and after_graph[nx][ny] != 0:
                     visited[nx][ny] = True
                     q.append((nx, ny))
 
-                if graph[nx][ny] == 0: # and melted_graph[qx][qy] < graph[qx][qy]:
-                    melted_graph[qx][qy] += 1
-
-    if is_visit:
-        return 1
-    else:
-        return 0
-
-
-# 1)
-count = 0
-year = 0
-melted = 0
 
 while True:
-    visited = [[False] * M for _ in range(N)]
-    # 빙하 개수 세기
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] > 0:
-                count += bfs(i, j)  # 빙하 개수 세기
-                print(count)
-
-            if count > 1:  # 빙하 개수가 2개 이상이라면
-                print(f'year is {year}')
-                sys.exit(0)
-
-    # 빙하가 없는 경우
-    if count <= 0:
-        print(f'nothing melt {0}')
-        sys.exit(0)
-
-    # # 빙하가 녹는 경우
-    # for i in range(N):
-    #     for j in range(M):
-    #         if graph[i][j] > 0:
-    #             melted += melt(i, j)
-
-    melt_v2()
-
     year += 1
+    iceberg_cnt = 0
+    visited = [[False] * M for _ in range(N)]  # 방문여부
+
+    # 1년 후 녹은 빙산
+    after_graph = after_iceberg(graph)
+
+    # bfs를 이용하여 빙산의 개수를 셈
+    for i in range(0, N):
+        for j in range(0, M):
+            if after_graph[i][j] > 0 and not visited[i][j]:
+                bfs(i, j)
+                iceberg_cnt += 1
+
+    graph = copy.deepcopy(after_graph)
+    if iceberg_cnt >= 2:
+        print(year)
+        break
+    elif iceberg_cnt == 0:  # 만일 빙산이 다 녹을 때까지 분리되지 않으면
+        print(0)
+        break
+
