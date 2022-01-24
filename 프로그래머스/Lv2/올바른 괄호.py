0d53da2f0e8716878641f1561e592b5089cd7441
hjun-park <phj0860@gmail.com>
2022-01-24 21:35:00 +0900

    [2021-01-24] 63 -> 64
    1. 구현 1

---
+++ 프로그래머스/Lv2/올바른 괄호.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solution(s):
+    count = 0
+
+    # 1) 처음 괄호가 닫혀있거나 마지막 괄호가 열려있는 경우 False
+    if s[0] == ')' or s[-1] == '(':
+        return False
+
+    for ss in s:
+
+        # 2) 열린 괄호일 경우 카운트 증가
+        if ss == '(':
+            count += 1
+
+        # 3) 닫힌 괄호일 경우
+        else:
+            count -= 1
+
+        # count가 0보다 닫는 괄호가 더 추가된 것이므로 False
+        if count < 0:
+            return False
+
+    # 만약 닫는괄호, 여는괄호 수가 같지 않다면 False
+    if count != 0:
+        return False
+
+    return True
+
+
+print(solution("()()"))
+print(solution("(())()"))
+print(solution(")()("))
+print(solution("(()("))
