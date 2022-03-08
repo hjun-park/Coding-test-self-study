82a66d4bf5e502d7ba47ab6d5f8362fc8b49026d
hjun-park <phj0860@gmail.com>
2022-03-08 21:17:48 +0900

    [2022-03-08] 15
    1. 시뮬레이션 2

--- [--]문제집 풀이/[05]시뮬레이션/[기본-G3]16985-Maaaaaaaze.py
+++ [--]문제집 풀이/[05]시뮬레이션/[기본-G3]16985-Maaaaaaaze.py
@@ -1,7 +1,88 @@
 import sys
+from collections import deque
+from itertools import permutations
 
 input = sys.stdin.readline
 
 graph = [[list(map(int, input().split())) for _ in range(5)] for _ in range(5)]
 
+# graph 내의 판을 섞어서 구성한 미로를 집어넣을 배열
+maze = [[[0] * 5 for _ in range(5)] for _ in range(5)]
 
+# =========================
+# BFS 전용
+# =========================
+# 위 좌 상 우 하 아래 시계 방향
+dx = [0, 0, -1, 0, 1, 0]
+dy = [0, -1, 0, 1, 0, 0]
+dz = [1, 0, 0, 0, 0, -1]
+
+exit_time = []
+
+'''
+ 문제풀이 핵심
+  1) 메인 함수 : permutation을 이용하여 미로 판 쌓는 순서를 정한다.
+  2) logic() : 재귀함수 이용하여 입구가 1이 나올 때까지 판을 90도 4번 회전한다.
+               나오면 다음 layer를 회전시키고 마지막 판까지 도달하면 출구 확인하여 BFS 순회
+'''
+
+
+def bfs():
+    q = deque()
+    q.append((0, 0, 0))
+
+    visited = [[[-1] * 5 for _ in range(5)] for _ in range(5)]
+    visited[0][0][0] = 0
+
+    while q:
+        z, x, y = q.popleft()
+
+        if z == 4 and x == 4 and y == 4:  # 출구 도달
+            exit_time.append(visited[z][x][y])
+            return
+
+        for d in range(6):
+            nz = z + dz[d]
+            nx = x + dx[d]
+            ny = y + dy[d]
+
+            if 0 <= nz < 5 and 0 <= nx < 5 and 0 <= ny < 5:
+                if visited[nz][nx][ny] == -1 and maze[nz][nx][ny] == 1:
+                    q.append((nz, nx, ny))
+                    visited[nz][nx][ny] = visited[z][x][y] + 1
+
+
+# 90도 시계방향으로 회전하는 함수
+def rotate(layer):
+    tmp = [[0] * 5 for _ in range(5)]
+
+    for j in range(5):
+        for k in range(5):
+            # tmp[5 - 1 - k][j] = maze[layer][j][k]
+            tmp[k][4 - j] = maze[layer][j][k]
+    maze[layer] = tmp
+
+
+# 입구 출구 검증
+def logic(layer):
+    # 마지막 층까지 도달한 경우 출구 확인
+    if layer == 5:
+        if maze[4][4][4]:  # 출구가 있다면
+            bfs()
+        return
+
+    for r in range(4):  # 4번까지 회전 가능
+        if maze[0][0][0]:  # 입구가 있다면
+            logic(layer + 1)  # 다음 층으로 검증
+        rotate(layer)
+
+
+# 순열에 따른 미로 판 쌓기
+for p in permutations([0, 1, 2, 3, 4]):
+    for i in range(5):
+        maze[p[i]] = graph[i]
+
+    # permu 다 채웠으면 logic 순환
+    logic(0)
+
+print(min(exit_time) if exit_time else -1)
---
+++ [--]문제집 풀이/[05]시뮬레이션/[기본-G5]14503-로봇 청소기.py
@@ -0,0 +1,62 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+r, c, d = map(int, input().split())
+graph = []
+
+# 좌표 설정 - 문제에서 요구하는대로 ( 북, 동, 남, 서 )
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 1) 그래프 입력 받기
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+
+# 2) 방향을 좌측으로 돌리는 함수
+def turn_left():
+    global d
+    d = (d - 1) % 4  # 1을 빼면 왼쪽으로, 더하면 오른쪽으로 돈다.
+
+
+# 3) count 1로 초기화(시작지 청소하고 시작하므로) 및 시작지 x, y로 초기화
+count = 1
+x, y = r, c
+
+# 0: 미청소, 1: 벽, 2: 청소처리(방문처리)
+# 4) 해당 시작 좌표 값은 2로 초기화 ( 청소 처리 )
+graph[x][y] = 2
+
+while True:
+    check = False  # 방문한 칸이 있는지 없는지 유무 파악 변수
+    # 5) 4방향을 돌면서 nx, ny를 지정하여 그래프를 벗어나는지 확인
+    # 5-1) 벗어나지 않는다면, count += 1, 그리고 방문처리(2)를 해 줌
+    for i in range(4):
+        turn_left()
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= nx < n and 0 <= ny < m:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = 2
+                count += 1
+                # 실제 이동
+                x, y = nx, ny
+                check = True  # 칸이 있다는 의미 처리
+                break
+
+    # 6) check값 확인, False라면 4방향 모두 이동을 못하는 경우이므로 nx, ny로 후진좌표 계산
+    if not check:
+        nx = x - dx[d]
+        ny = y - dy[d]
+        if 0 <= nx < n and 0 <= ny < m:
+            if graph[nx][ny] == 2:  # 청소한 자리인 경우 이동
+                x, y = nx, ny
+            elif graph[nx][ny] == 1:  # 벽인 경우 출력, 종료
+                print(count)
+                break
+
+    # 6-1) 후진좌표가 접근 가능한 경우 (이미 청소한 칸인 경우 후진)
+    # 6-2) 후진좌표가 1인 경우 즉, 벽인 경우는 count출력하고 끝낸다.
+    # 6-3) 그 이외의 경우도 종료한다.
