b4ff9beb410faf3e69cf9584db366a4962152bfa
hjun-park <phj0860@gmail.com>
2023-01-30 23:27:19 +0900

    [2023-01-30] dp - 주석 필요

--- _cop/[10]DP/[S1]12852-1로 만들기.py
+++ _cop/[10]DP/[S1]12852-1로 만들기.py
@@ -4,31 +4,30 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-'''
-1. dp 함수 정의
-
-2. 점화식 추론
-
-'''
-
 d = [0] * (N + 2)
-trace = []
+pre = [0] * (N + 2)  # 이전 수 추적용
+
 
 def logic():
     for i in range(2, N + 1):
         d[i] = d[i - 1] + 1
-        trace.append(i-1)
+        pre[i] = i - 1
 
-        if i % 3 == 0:
-            d[i] = min(d[i // 3] + 1, d[i])
-            trace.pop()
-            trace.append(i//3)
-        if i % 2 == 0:
-            d[i] = min(d[i // 2] + 1, d[i])
-            trace.pop()
-            trace.append(i//2)
+        if i % 3 == 0 and d[i // 3] + 1 < d[i]:
+            d[i] = d[i // 3] + 1
+            pre[i] = i // 3
+
+        if i % 2 == 0 and d[i // 2] + 1 < d[i]:
+            d[i] = d[i // 2] + 1
+            pre[i] = i // 2
 
 
 logic()
 print(d[N])
-print(*trace)
+cur = N
+while True:
+    print(cur, end=' ')
+    if cur == 1:
+        break
+
+    cur = pre[cur]
