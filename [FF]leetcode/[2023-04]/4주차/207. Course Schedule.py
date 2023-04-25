510869165991cb7b23f3184c060f0229ee8e352b
hjun-park <phj0860@gmail.com>
2023-04-25 21:08:37 +0900

    [2023-04-25] (Course Schedule) - 위상정렬 알고리즘

--- [FF]leetcode/[2023-04]/4주차/207. Course Schedule.py
+++ [FF]leetcode/[2023-04]/4주차/207. Course Schedule.py
@@ -1,63 +1,71 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import List
 
-# 참고링크
-# 1) https://leetcode.com/problems/course-schedule/solutions/3397439/python-toposort-simple-and-clean-beats-88-98/
-# 2) https://leetcode.com/problems/course-schedule/solutions/441722/python-99-time-and-100-space-collection-of-solutions-with-explanation/
-# 3) https://leetcode.com/problems/course-schedule/solutions/658379/python-by-dfs-and-cycle-detection-w-graph/
-# 4) https://leetcode.com/problems/course-schedule/solutions/368508/python3-breadth-first-search-for-cycle-detection/
-
-
 class Solution:
-    # 1. 필요 변수 선언
-    visited = []
-    graph = defaultdict(list)
-
-    def dfs(self, start, end):
-        # 1. (base condition) 만약 가려던 곳을 이미 갔다면
-        if self.visited[end]:
-            # 1. 시작지와 다음 도착지가 같다면 사이클
-            if start == end:
-                return False
-
-        # 2. 방문처리
-        self.visited[start] = True
-
-        # 3. 현재 상태에서 갈 수 있는 곳들을 순차적으로 돈다.
-        for v in self.graph[end]:
-            # 1. DFS 순회
-            if self.dfs(start, v):
-                return False
-
-        return True
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # 1. 수강과목이 하나인 경우 true
-        if numCourses <= 1 or len(prerequisites) == 0:
-            return True
 
-        # 2. 수강표를 따라감
-        for p in prerequisites:
-            v, w = p
-            # 1. w를 먼저 들어야만 v를 들을 수 있다.
-            self.graph[w].append(v)
+        # 3. 위상정렬 시작
+        def topo_sort():
+            result = []  # 위상정렬 결과
 
-        # 3. visited 배열 생성
-        self.visited = [False] * numCourses
+            # 1. 위상정렬용 큐 생성
+            q = deque()
 
-        # 4. numCourses 개수만큼 dfs 순회
-        for n in range(numCourses):
-            if self.dfs(n, n):
-                return True
+            # 2. 노드 개수만큼 돌면서 indegree가 0인 경우만 q에 담는다.
+            for i in range(numCourses):
+                if indegree[i] == 0:
+                    q.append(i)
 
-        # 5. 다 순회한 경우 사이클이 없다는 것을 의미
-        return False
+            # 3. q 순회 시작
+            while q:
+                now = q.popleft()
+
+                # 1. 가장 먼저 방문한 노드는 결과에 포함
+                result.append(now)
+
+                # 2. 현재 꺼낸 노드가 갈 수 있는 모든 방향을 순회
+                for v in graph[now]:
+                    # 1. 간선을 제거 (간선 제거되면 차수 감소)
+                    indegree[v] -= 1
+
+                    # 2. 간선 제거했는데 차수가 0이라면 q에 추가
+                    if indegree[v] == 0:
+                        q.append(v)
+
+            # 4. q가 다 끝나면 위상 정렬 완료된 상태
+            print(result)
+
+            # 5. 위상 정렬 결과에 노드가 전부 없다면 해당 그래프는 사이클이 존재하는 상태
+            # 사이클 존재 = 해당 강의 전부 들을 수 없다.
+            return len(result) == numCourses
+
+        indegree = [0] * (numCourses + 1)  # 차수
+        graph = defaultdict(list)  # 그래프
+
+        # 1. 과목 edges 순회
+        for a, b in prerequisites:
+            # 1. b를 들어야 a를 들을 수 있다.
+            graph[b].append(a)
+
+            # 2. b -> a로 들어가므로 a의 indegree 증가
+            indegree[a] += 1
+
+        # 2. 위상 정렬 시작
+        return topo_sort()
 
 
 s = Solution()
-# print(s.canFinish(2, [[1, 0]]))  # true
-# print(s.canFinish(2, [[1, 0], [0, 1]]))  # false
-# print(s.canFinish(4, [[3, 2], [1, 0]]))  # true
-# print(s.canFinish(4, [[3, 1], [0, 2]]))  # true
-# print(s.canFinish(1, []))  # true
 print(s.canFinish(2, [[1, 0]]))  # true
+print(s.canFinish(2, [[1, 0], [0, 1]]))  # false
+print(s.canFinish(4, [[3, 2], [1, 0]]))  # true
+print(s.canFinish(4, [[3, 1], [0, 2]]))  # true
+print(s.canFinish(1, []))  # true
+print(s.canFinish(2, [[1, 0]]))  # true
+
+
+'''
+문제 핵심
+1. 사이클이 있다면 해당 강의 전부 들을 수 없다.
+2. 사이클이 없어야만 돌 수 있는 위상정렬을 이용한다.
+'''
